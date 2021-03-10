1e1c957c0f708d2ffad057ff3c93d1051f4fb1b3
yoni206 <yoni206@gmail.com>
2021-03-10 00:31:30 -0800

    a python example

--- tests/btor/btor-example.cpp
+++ tests/btor/btor-example.cpp
@@ -1,40 +1,51 @@
 #include <iostream>
 #include "boolector_factory.h"
 #include "smt.h"
-using namespace smt; using namespace std;
-
+using namespace smt; 
+using namespace std;
 int main()
 {
-  SmtSolver s = BoolectorSolverFactory::create(true);
-  s->set_logic("QF_UFBV");
-  s->set_opt("incremental", "true");
-  s->set_opt("produce-models", "true");
-  s->set_opt("produce-unsat-assumptions", "true");
-  Sort bvsort = s->make_sort(BV, 32);
-  Sort funsort = s->make_sort(FUNCTION, {bvsort, bvsort});
+ SmtSolver s = 
+   BoolectorSolverFactory::create(true);
+ s->set_logic("QF_UFBV");
+ s->set_opt("incremental", "true");
+ s->set_opt("produce-models", "true");
+ s->set_opt("produce-unsat-assumptions", 
+   "true");
+ Sort bvs = s->make_sort(BV, 32);
+ Sort funs = 
+   s->make_sort(FUNCTION, {bvs, bvs});
 
-  Term x = s->make_symbol("x", bvsort);
-  Term y = s->make_symbol("y", bvsort);
-  Term f = s->make_symbol("f", funsort);
-  Term fx = s->make_term(Apply, f, x);
-  Term fy = s->make_term(Apply, f, y);
-  Term x0 = s->make_term(Op(Extract, 15, 0), x);
-  Term y0 = s->make_term(Op(Extract, 15, 0), y);
-  
-  s->assert_formula(s->make_term(Distinct, fx, fy));
+ Term x = s->make_symbol("x", bvs);
+ Term y = s->make_symbol("y", bvs);
+ Term f = s->make_symbol("f", funs);
+ Term fx = s->make_term(Apply, f, x);
+ Term fy = s->make_term(Apply, f, y);
+ 
+ Op ext = Op(Extract, 15, 0);
+ Term x0 = s->make_term(ext, x);
+ Term y0 = s->make_term(ext, y);
 
-  s->push(1);
-  s->assert_formula(s->make_term(Equal, x0, y0));
-  cout <<  s->check_sat() << endl;
-  cout << s->get_value(x) << endl;
-  s->pop(1);
+ s->assert_formula(
+   s->make_term(Distinct, fx, fy));
 
-  Term x_and_y = s->make_term(BVAnd, x, y);
-  Term a1 = s->make_term(BVUge, x_and_y, x);
-  Term a2 = s->make_term(BVUge, x_and_y, y);
-  Term a3 = s->make_term(BVUge, x0, y0);
-  cout << s->check_sat_assuming({a1, a2, a3}) << endl;
-  UnorderedTermSet ua; 
-  s->get_unsat_assumptions(ua); 
-  for (Term t : ua) { cout << t << endl; }
+ s->push(1);
+ s->assert_formula(
+   s->make_term(Equal, x0, y0));
+ cout <<  s->check_sat() << endl;
+ 
+ cout << s->get_value(x) << endl;
+ s->pop(1);
+
+ Term xy = s->make_term(BVAnd, x, y);
+ Term a1 = s->make_term(BVUge, xy, x);
+ Term a2 = s->make_term(BVUge, xy, y);
+ Term a3 = s->make_term(BVUge, x0, y0);
+ cout << 
+   s->check_sat_assuming({a1, a2, a3}) 
+   << endl;
+ UnorderedTermSet ua;
+ s->get_unsat_assumptions(ua);
+ for (Term t : ua) { cout << t << endl; }
 }
+
